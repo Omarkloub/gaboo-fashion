2a7d9ea9d3e946fc16322817c416f9dc98e3aa6e
Omar kloub <136655508+Omarkloub@users.noreply.github.com>
2023-10-27 16:24:38 +0300

    Add files via upload

---
+++ gaboo!.js
@@ -0,0 +1,17 @@
+document.getElementById("button").addEventListener("click", function(){
+    document.querySelector(".loginW").style.display = "flex";
+
+})
+document.querySelector(".close").addEventListener("click", function(){
+document.querySelector(".loginW").style.display = "none"
+})
+ 
+  
+document.getElementById("loginop1").addEventListener("click", function(){
+    document.querySelector(".loginW_1").style.display = "flex";
+
+})
+document.querySelector(".close_1").addEventListener("click", function(){
+document.querySelector(".loginW_1").style.display = "none"
+
+})
